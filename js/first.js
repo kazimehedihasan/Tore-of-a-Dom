740d7a1e50702ee0f3146e5252910923d21622c5
kazimehedihasan <kazimehedihasan458@.com>
2023-08-12 21:19:16 +0600

    first commit

---
+++ js/first.js
@@ -0,0 +1,22 @@
+console.log('first wed sits');
+
+
+const allLi = document.getElementsByTagName('li');
+console.log(allLi);
+
+const allTitles = document.getElementsByClassName('section-title');
+console.log(allTitles);
+
+const sectionSection = document.getElementById('second-section');
+console.log(sectionSection);
+sectionSection.style.color = 'red';
+sectionSection.style.background = 'black';
+
+// document. querySelectorAll
+// document. querySelector
+
+
+
+
+
+
---
+++ js/tharid.js
@@ -0,0 +1,15 @@
+const main = document.getElementById('main-container');
+const section = document.createElement('section');
+section.innerHTML = `
+<h1>My dynamic section</h1>
+<p>Extra text added insid paragraph</p>
+<ul>
+<li>first item</li>
+<li>second item</li>
+<li>thaird item</li>
+<li>fort item</li>
+<li>five item</li>
+<li>six item</li>
+</ul>
+
+`
